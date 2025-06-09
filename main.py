12af7be491e449bf748960366d6450ff0c73f5ae
Andrew B <mrariec007@gmail.com>
2025-06-09 16:54:58 +0300

    add password for excel file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from time import sleep
 from datetime import datetime
 
+
 # Функция для отправки сообщения на почту
 def send_message_to_me(sender, message):
     mail = outlook.CreateItem(0)
@@ -21,9 +22,9 @@ def print_with_time(text):
 
 
 # Функция для обновления файла (Синтаксис из VBA, за счёт библиотеки)
-def update_excel_file(path_file):
+def update_excel_file(path_file, excel_pass):
     xlapp = win32.DispatchEx("Excel.Application")
-    wb = xlapp.Workbooks.Open(path_file)
+    wb = xlapp.Workbooks.Open(path_file, 2, False, None, None, excel_pass)
     xlapp.Visible = False
     wb.RefreshAll()
     xlapp.CalculateUntilAsyncQueriesDone()
@@ -33,10 +34,10 @@ def update_excel_file(path_file):
 
 
 # Функция полного обновления отчётности
-def update_all(my_excel_files):
+def update_all(my_excel_files, excel_pass):
     for excel_file in my_excel_files:
         print_with_time(f"Обновляем файл {excel_file.split('\\')[-1]}")
-        update_excel_file(excel_file)
+        update_excel_file(excel_file, excel_pass)
 
     # отправляем сообщение
     print_with_time("Сообщение отправлено")
@@ -45,24 +46,38 @@ def update_all(my_excel_files):
 
 # outlook = win32.Dispatch("outlook.application")
 
-print_with_time("Данный скрипт предназначен для автоматического обновления отчётов Excel")
-print_with_time("В папке с этим скриптом должен находится один текстовый файл с путями к excel файлам")
+print_with_time(
+    "Данный скрипт предназначен для автоматического обновления отчётов Excel"
+)
+print_with_time(
+    "В папке с этим скриптом должен находится один текстовый файл с путями к excel файлам"
+)
 
-mail = input(f"[{datetime.now().strftime('%d/%m/%Y, %H:%M')}] Введите почту на которую будет отправленно сообщение: ")
+mail = input(
+    f"[{datetime.now().strftime('%d/%m/%Y, %H:%M')}] Введите почту на которую будет отправленно сообщение: "
+)
+
+pass_excel = input(
+    f"[{datetime.now().strftime('%d/%m/%Y, %H:%M')}] Введите пароль для файлов (Если нет, то пропустите): "
+)
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 while True:
-    txt_files = [f for f in os.listdir(current_dir) if f.endswith('.txt')]
+    txt_files = [f for f in os.listdir(current_dir) if f.endswith(".txt")]
 
     if not txt_files:
         print_with_time("Нет текстовых файлов")
     elif len(txt_files) == 1:
         print_with_time("Найден файл {txt_files[0]}")
         file_path = os.path.join(current_dir, txt_files[0])
-        with open(file_path, 'r', encoding='utf-8') as file:
-            excel_files = [e.strip() for e in file.readlines() if os.path.exists(e.strip()) and e.strip().endswith('.xlsx')]
-        
+        with open(file_path, "r", encoding="utf-8") as file:
+            excel_files = [
+                e.strip()
+                for e in file.readlines()
+                if os.path.exists(e.strip()) and e.strip().endswith(".xlsx")
+            ]
+
         if not excel_files:
             print_with_time("В файле нет корректный путей.")
         else:
@@ -70,7 +85,7 @@ while True:
             break
     else:
         print_with_time("Файлов два и более, оставьте один")
-    
+
     sleep(15)
 
-update_all(excel_files)
+update_all(excel_files, pass_excel)
